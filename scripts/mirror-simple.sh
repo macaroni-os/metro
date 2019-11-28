6a8d55bb86b704757454f0363977b49a07249128
Daniel Robbins <drobbins@funtoo.org>
2019-11-27 19:25:05 -0700

    Improve mirroring script to keep all old files in place until all new files are uploaded.

--- scripts/mirror-simple.sh
+++ scripts/mirror-simple.sh
@@ -12,6 +12,11 @@ if [ -z "$mp" ]; then
 else
 	echo "Mirroring $mp..."
 fi
-rsync -rltJOve ssh --partial --progress --delete --exclude *.cme --exclude *.cme.run --exclude stage1*.tar* --exclude stage2*.tar* --exclude *.tar $mp/1.4-release-std/ drobbins@upload.funtoo.org:/home/mirror/funtoo/1.4-release-std/
+rsync -rltJOve ssh --partial --progress --exclude *.cme --exclude *.cme.run --exclude stage1*.tar* --exclude stage2*.tar* --exclude *.tar $mp/1.4-release-std/ drobbins@upload.funtoo.org:/home/mirror/funtoo/1.4-release-std/
 rsync -rltJOve ssh --partial --progress --delete --exclude *.tar $mp/livecd drobbins@upload.funtoo.org:/home/mirror/funtoo/
 ssh drobbins@upload.funtoo.org sudo /root/metro/scripts/buildrepo index.xml
+# don't delete until we've reindexed.
+rsync -rltJOve ssh --partial --progress --exclude *.cme --exclude *.cme.run --exclude stage1*.tar* --exclude stage2*.tar* --exclude *.tar $mp/1.4-release-std/ drobbins@upload.funtoo.org:/home/mirror/funtoo/1.4-release-std/ --delete
+# reindex again.
+ssh drobbins@upload.funtoo.org sudo /root/metro/scripts/buildrepo index.xml
+
