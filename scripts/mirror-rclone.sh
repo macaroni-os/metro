84e27678eac822cf6126ac48be801fcd34b9cfe4
Daniel Robbins <drobbins@funtoo.org>
2020-12-22 15:52:27 -0700

    Remove hardcoded paths in mirror-rclone to make it work with centralized config/code.

--- scripts/mirror-rclone.sh
+++ scripts/mirror-rclone.sh
@@ -4,7 +4,6 @@
 #CUSTOM_EXCLUDE="--exclude *2020-02-03*"
 metro="$(dirname $0)/../metro"
 mp=$($metro -k path/mirror 2>/dev/null)
-echo $mp
 if [ -z "$mp" ]; then
 	echo "Could not get path/mirror from metro configuration; exiting."
 	exit 1
@@ -13,9 +12,9 @@ else
 fi
 EXTRA_EXCLUDES="--exclude=selinux* --exclude=*2019-* --exclude=*2020-01* --exclude=*2020-02* --delete-excluded"
 EXCLUDES="--exclude *.cme --exclude *.cme.run --exclude *.progress --exclude stage1*.tar* --exclude stage2*.tar* --exclude *.tar"
-/root/metro/scripts/buildrepo index.xml
-/root/metro/scripts/indexr.py /mnt/data/mirror/funtoo
-rclone -Pl $EXCLUDES $EXTRA_EXCLUDES --b2-chunk-size=36M --b2-upload-cutoff=36M --transfers=18 --checkers=24 sync /mnt/data/mirror/funtoo b2:funtoo-mirror
+$mp/../metro/scripts/buildrepo index.xml
+$mp/../metro/scripts/indexr.py $mp
+rclone -Pl $EXCLUDES $EXTRA_EXCLUDES --b2-chunk-size=36M --b2-upload-cutoff=36M --transfers=18 --checkers=24 sync $mp b2:funtoo-mirror
 rclone cleanup b2:funtoo-mirror
 if [ "$1" == "half" ]; then
 	echo "Only performing first half of mirroring operation."
