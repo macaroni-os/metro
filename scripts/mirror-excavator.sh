4f8c78e6dab34a021856f1cb21ef9e341f317829
Daniel Robbins <drobbins@funtoo.org>
2024-07-07 16:01:56 -0600

    Use GNU parallel to speed up excavator transfers more.

--- scripts/mirror-excavator.sh
+++ scripts/mirror-excavator.sh
@@ -2,6 +2,11 @@
 method="rsync"
 metro="$(dirname $0)/../metro"
 mp=$($metro -k path/mirror 2>/dev/null)
+which parallel > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "This script requires GNU parallel. Emerge sys-process/parallel first."
+	exit 1
+fi
 if [ -z "$mp" ]; then
 	echo "Could not get path/mirror from metro configuration; exiting."
 	exit 1
@@ -15,5 +20,5 @@ EXCLUDES="--exclude *.cme.run --exclude *.progress --exclude stage1*.tar* --excl
 #$mp/../metro/scripts/indexr.py $mp
 for x in amd64-bulldozer amd64-excavator amd64-steamroller amd64-piledriver amd64-k10; do
 	dest=next/x86-64bit/$x
-	rsync -azve ssh --zc zstd --zl 6 --delete $mp/$dest/ root@172.19.1.20:/home/mirror/funtoo/$dest $EXCLUDES $EXTRA_EXCLUDES --progress --stats
-done
+	echo rsync -azve ssh --zc zstd --zl 6 --delete $mp/$dest/ root@172.19.1.20:/home/mirror/funtoo/$dest $EXCLUDES $EXTRA_EXCLUDES
+done | parallel
